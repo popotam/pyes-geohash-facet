9f0690e9cd091141bb82ef6c40eefa92e587f200
Paweł Sobkowiak <pawel.sobkowiak@gmail.com>
2014-08-13 16:28:13 +0200

    Support both 0.0.17 and 0.0.13-przemek versions

--- pyes_geohash_facet/facet.py
+++ pyes_geohash_facet/facet.py
@@ -7,7 +7,10 @@ class GeohashFacet(facets.Facet):
     def __init__(self, name, **kwargs):
         self.field = kwargs.pop('field', None)
         self.factor = kwargs.pop('factor', 0)
-        self.show_doc_id = kwargs.pop('show_doc_id', False)
+        self.show_doc_id = (
+            kwargs.pop('show_doc_id', False)
+            or kwargs.pop('show_documents', False)
+        )
         self.show_geohash_cell = kwargs.pop('show_geohash_cell', False)
 
         super(GeohashFacet, self).__init__(name, **kwargs)
@@ -17,6 +20,7 @@ class GeohashFacet(facets.Facet):
 
         data['field'] = self.field
         data['factor'] = self.factor
+        data['show_documents'] = self.show_doc_id
         data['show_doc_id'] = self.show_doc_id
         data['show_geohash_cell'] = self.show_geohash_cell
 
